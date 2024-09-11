4dca3f13a879c763acb86419a7d40a63822b9fb2
Rebekah <macrosystem@proton.me>
2024-09-11 17:52:53 +0200

    Update and rename 01-basic.py to 01-mvp.py

--- python-garfield-quiz/01-mvp.py
+++ python-garfield-quiz/01-mvp.py
@@ -12,12 +12,15 @@ if( input("what day does garfield hate?") == "monday" OR "mondays"):
 else:
     print("wrong :(")
 
-print("quiz complete!! your score is: " + str(score))
+print("quiz complete!! your score is: ")
+print(score)
 
 """not important for now: list of things to add:
 answer_check_function()
+str(score)
 lower(input())
 read questions and answers from a list (2d list / dict / tuple)
 """
 
 #fixme: untested
+#mvp --> minimal viable product
