bc0483932280f4d2257a1a4998b0067477f63cd7
Rebekah <macrosystem@proton.me>
2024-09-11 16:00:34 +0200

    Update python-garfield-quiz.py

--- python-garfield-quiz.py
+++ python-garfield-quiz.py
@@ -1,5 +1,23 @@
 score = 0
 
-if( input("what is garfields favorite food?) == "Lasagna"):
-          score += 1
-          print("correct!")
+if( input("what is garfields favorite food?") == "lasagna"):
+    score += 1
+    print("correct!")
+else:
+    print("wrong :(")
+
+if( input("what day does garfield hate?") == "monday" OR "mondays"):
+    score += 1
+    print("correct!")
+else:
+    print("wrong :(")
+
+print("quiz complete!! your score is: " + str(score))
+
+"""not important for now: list of things to add:
+answer_check_function()
+lower(input())
+read questions and answers from a list (2d list / dict / tuple)
+"""
+
+#fixme: untested
