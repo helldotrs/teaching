03f77d5a237e8d3d356c8ff47486b47b42dbddf9
Rebekah <macrosystem@proton.me>
2024-09-11 18:07:37 +0200

    Update and rename v02-functions.py to v03-str().py

--- python-garfield-quiz/v03-str().py
+++ python-garfield-quiz/v03-str().py
@@ -1,3 +1,7 @@
+#By converting the score from int (positive whole number) to string (text) we can now combine it in the same print statement without mixing types. 
+#Notice that in programming*, 7 written as such is a number while "7" written as such is a text. hence 7+7 --> 14, "7"+"7" --> "77"
+#*any exceptions to this are in garbage programming languages that should for the better part be disregarded
+
 score = 0
 
 def ask_question(question, correct_answer):
@@ -9,15 +13,13 @@ def ask_question(question, correct_answer):
     else:
         print("wrong answer :(")
       
-    print("your current score is: ")
-    print(score)
+    print("your current score is: " + str(score))
 
 
 ask_question("what is garfields favorite food?", "lasagna")
 ask_question("what day does garfield hate?", "monday")
 
-print("quiz complete!! your score is: ")
-print(score)
+print("quiz complete!! your final score is: " str(score))
 
 """not important for now: list of things to add:
 str(score)
